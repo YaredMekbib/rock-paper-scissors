de8e9c06ff4ea239b0c30836d8f9654b7ea3cc79
YaredMekbib <yared.mekbib@gmail.com>
2025-08-07 12:10:51 -0700

    Aded getComputerChoice function that determines whether the computer picks rock, scissors, or paper

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const x = Math.random();
+
+    if (x < 1/3) {
+        console.log("Rock");
+    }
+    else if (x < 2/3) {
+        console.log("Scissors");
+    }
+    else {
+        console.log("Paper");
+    }
+    
+}
+console.log(getComputerChoice());
