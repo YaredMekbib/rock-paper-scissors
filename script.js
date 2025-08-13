9ea93925bb97e9298c273d7f48119bbc8aebdf1d
YaredMekbib <yared.mekbib@gmail.com>
2025-08-13 13:39:05 -0700

    Changed all console.logs to returns. Added return choice in the function getHumanChoice. Created a function called playRound with parameters of humanChoice and computerChoice. Filled that function with various if, else if, and else statements to record who wins each round and the dynamics of each round.

--- script.js
+++ script.js
@@ -2,21 +2,50 @@ function getComputerChoice() {
     const x = Math.random();
 
     if (x < 1/3) {
-        console.log("Rock");
+        return("Rock");
     }
     else if (x < 2/3) {
-        console.log("Scissors");
+        return("Scissors");
     }
     else {
-        console.log("Paper");
+        return("Paper");
     }
     
 }
 
 function getHumanChoice() {
     let choice = prompt("Rock, Paper, or Scissors?")
+    return choice;
 }
 userScore = 0;
 computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
 
+    if (humanChoice === computerChoice){
+        return ("Its a tie!");
+    }
+    else if
+    (   (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock"))
+        {
+            userScore = userScore + 1;
+            return ("One point for the user!");
+        }
+
+    else if
+    (   (computerChoice === "rock" && humanChoice === "scissors") ||
+        (computerChoice === "scissors" && humanChoice === "paper") ||
+        (computerChoice === "paper" && humanChoice === "rock")) 
+        {
+            computerScore = computerScore + 1;
+            return ("One point for the computer!");
+        }
+        
+    else {
+        return ("Invalid input. Please choose Rock, Paper, or Scissors");
+    }
+}
